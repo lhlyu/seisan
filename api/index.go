29b076fdd27a06db5e9fd48ef17a213a3341b942
lhlyu <misakafs@gmail.com>
2022-02-20 11:48:02 +0800

    验证

--- api/index.go
+++ api/index.go
@@ -3,19 +3,26 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/spf13/cast"
 	"io/ioutil"
 	"net/http"
 )
 
 var s string
 
+const a = 1
+
 func init() {
-	b, _ := ioutil.ReadFile("./demo.txt")
+	b, err := ioutil.ReadFile("./demo.txt")
+	if err != nil {
+		s = err.Error()
+		return
+	}
 	s = string(b)
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
 	header, _ := json.Marshal(r.Header)
-	fmt.Fprintf(w, `{"hello":"%s", "header"}`, s, string(header))
+	fmt.Fprintf(w, `{"a": "%s","hello":"%s", "header": "%s"}`, cast.ToString(a), s, string(header))
 }
