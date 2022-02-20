1e371e2d5a83987ef6da2c5baa301e87b3f523e5
lhlyu <misakafs@gmail.com>
2022-02-20 12:34:09 +0800

    验证

--- internal/demo/demo.go
+++ internal/demo/demo.go
@@ -12,10 +12,12 @@ const a = 1
 
 func init() {
 
-	b, err := ioutil.ReadFile("../internal/demo/demo.txt")
+	b, err := ioutil.ReadFile("./demo.txt")
 	if err != nil {
-		S = err.Error()
-		return
+		b, err = ioutil.ReadFile("./internal/demo/demo.txt")
+		if err != nil {
+			S = err.Error()
+		}
 	}
 	S = string(b)
 }
