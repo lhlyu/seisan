23fdcb1522158734ac962050c8f9ef9bde4f96aa
lhlyu <misakafs@gmail.com>
2022-02-20 14:26:09 +0800

    验证

--- api/index.go
+++ api/index.go
@@ -2,30 +2,27 @@ package api
 
 import (
 	"net/http"
-	"seisan/internal/model"
+	"seisan/internal/core"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-	req := model.NewReq(r)
+	req := core.NewReq(r)
 	if !req.Check() {
-		resp := model.NewErrorResp("签名校验失败")
-		resp.Write(w)
-		return
+		//resp := core.NewErrorResp("签名校验失败")
+		//resp.Write(w)
+		//return
 	}
-	if req.Query.Kind == "" {
-		resp := model.NewErrorResp("参数不合法")
-		resp.Write(w)
-		return
-	}
-	//data, err := core.GetResult(req.Query)
-	//if err != nil {
-	//	resp := model.NewErrorResp(err.Error())
+	//if req.Query.Kind == "" {
+	//	resp := core.NewErrorResp("参数不合法")
 	//	resp.Write(w)
 	//	return
 	//}
-	//resp := model.NewResp(data)
-	//resp.Write(w)
-	resp := model.NewErrorResp("无")
+	data, err := core.GetResult(req)
+	if err != nil {
+		resp := core.NewErrorResp(err.Error())
+		resp.Write(w)
+		return
+	}
+	resp := core.NewResp(data)
 	resp.Write(w)
-	return
 }
